4e06f2a177427dd3cd97674571b1cc017a9e330c
ebubeikechi <ebubeikechi@gmail.com>
2023-03-17 08:26:00 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a progran that prints a line using prinf function
+ * Description: using printf function to print a line
+ * return: 0 (success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
