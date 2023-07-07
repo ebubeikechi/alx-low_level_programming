8d5cd8f2c47e04eafe27d8c84c01771922195378
ebubeikechi <ebubeikechi@gmail.com>
2023-07-07 12:00:54 -0700

    function to change binary to decimal, 0-main.c, README.md and main.h files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts binary numbers to decimal
+ * @b: holds the binary number to be covberted
+ * Return: result
+ **/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	while (*b != '\0')
+	{
+		if (*b == '0' || *b == '1')
+		{
+			result = result * 2 + (*b - '0');
+			b++;
+		} else
+		{
+			return (0);
+		}
+	}
+	return (result);
+}
